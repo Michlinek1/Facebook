e4dc2b56024b3d0c5c01b56291c250a29511571f
Michlinek1 <94484725+Michlinek1@users.noreply.github.com>
2022-09-15 16:19:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,14 @@ $_SESSION['login'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Facebuk lepsza wersja</title>
-    <link rel = "Stylesheet" href = "style.css">
+    <link rel = "stylesheet" href = "style.css">
     </head>
 <body>
     <?php
         if ($_SESSION['zalogowany'] == TRUE){
     echo '<ul class="nav">';
     echo ' <li><a href="index.php">Strona główna</a></li>';
+    echo ' <li><a href="main.php">Posty</a></li>';
     echo '  <li><a href="wyloguj.php">Wyloguj się</a></li>';
     echo '</ul>';
     }else{
@@ -25,7 +26,8 @@ $_SESSION['login'];
     echo ' <li><a href="index.php">Strona główna</a></li>';
     echo ' <li><a href="logowanie.php">Zaloguj się</a></li>';
     echo '  <li><a href="rejestracja.php">Zarejestruj się</a></li>';
-        }
+    
+}
     ?>
 <?php
 if($_SESSION['zalogowany'] == true){
